bbc5ae093f1d52ff69bd46782529a6b849a48b0c
marcus bianchi <marcusbianchi92@hotmail.com>
2021-07-07 12:01:38 -0300

    Adicionar arquivos de projeto.

---
+++ 09.Manual.Exercise.02.ENUMS/Entities/Departament.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Entities {
+    class Departament {
+        public string Name { get; set; }
+        public Departament() { }
+        public Departament(string name) {
+            Name = name;
+        }
+    }
+}
